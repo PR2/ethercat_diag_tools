5584f34057fdf84173ef55d68fdcd413639696c1
dking <dking@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2011-11-03 02:18:56 +0000

    Allow events to be expanded/colapsed in list.

--- diagnostic_annotate/scripts/event_viewer.py
+++ diagnostic_annotate/scripts/event_viewer.py
@@ -106,14 +106,56 @@ def pretty_duration(duration):
     return result
 
 
+class EventGroup:
+    def __init__(self, event, indent):
+        self.indent = indent
+        self.show = True if (indent == 0) else False
+        self.event = event
+        self.subgroups = []
+        for event in event.children:
+            self.subgroups.append( EventGroup(event,indent+1) )
+
+    def generateVisibleList(self):
+        results = [self]
+        for group in self.subgroups:
+            if group.show:
+                results += group.generateVisibleList()
+        return results
+
+    def showChildren(self, show):
+        for group in self.subgroups:
+            group.show = show
+
+    def generateGridData(self, ref_time):
+        event = self.event
+        localtime = time.localtime(event.t.to_sec())
+        indent_str = ('-' * self.indent) + '+'        
+        duration_str = pretty_duration((event.t-ref_time).to_sec())
+        time_str = time.strftime("%I:%M.%S %p", localtime)
+        date_str = time.strftime("%a, %b %d %Y", localtime)
+        child_count = EventViewerFrame.sumChildren(event)        
+        row_data = [indent_str, duration_str, date_str, time_str, event.type, event.name, str(child_count), event.desc, str(event.data)]
+        return row_data
+
+
+
 
 class EventViewerFrame(wx.Frame):
     def __init__(self, events, input_filename=""):
         wx.Frame.__init__(self, None, -1, "Event Viewer")
 
         self.input_filename = input_filename
-        self.events = events
-        self.current_selection = -1
+        #self.events = events
+        self.event_group_selection = None
+        if len(events) > 0:
+            self.ref_time = events[0].t
+        else:
+            self.ref_time = rospy.Time(0)
+        self.event_groups = [EventGroup(event,0) for event in events]        
+        self.visible_event_groups = None
+
+        if len(events) == 0:
+            displayErrorDialog(self, "No events")
 
         # Setting up the menu.
         filemenu= wx.Menu()
@@ -138,61 +180,39 @@ class EventViewerFrame(wx.Frame):
         hsizer.Add(self.set_ref_button, 0, wx.EXPAND)
         hsizer.Add(self.view_children_button, 0, wx.EXPAND)
         hsizer.Add(self.filename_textctrl, 1, wx.EXPAND)
-
-
+        
         grid = wx.grid.Grid(self)
         self.grid = grid
         self.Bind(wx.grid.EVT_GRID_SELECT_CELL, self.onSelectGridCell, self.grid)
-
-        num_cols = 8
+        
+        num_cols = 9
         grid.CreateGrid(0,num_cols)
 
-        grid.SetColLabelValue(0, "Relative\nTime")
-        grid.SetColLabelValue(1, "Date")
-        grid.SetColLabelValue(2, "Time")
-        grid.SetColLabelValue(3, "Event\nType")
-        grid.SetColLabelValue(4, "Component\nName");
-        grid.SetColLabelValue(5, "Total\nChildren")
-        grid.SetColLabelValue(6, "Description")
-        grid.SetColLabelValue(7, "Data")
+        grid.SetColLabelValue(0, "Indent")
+        grid.SetColLabelValue(1, "Relative\nTime")
+        grid.SetColLabelValue(2, "Date")
+        grid.SetColLabelValue(3, "Time")
+        grid.SetColLabelValue(4, "Event\nType")
+        grid.SetColLabelValue(5, "Component\nName");
+        grid.SetColLabelValue(6, "Total\nChildren")
+        grid.SetColLabelValue(7, "Description")
+        grid.SetColLabelValue(8, "Data")
 
         #header_grid.SetRowLabelSize(0) # hide the row labels
         grid.SetRowLabelSize(0) # hide the row labels
-
         grid.SetColLabelSize(grid.GetDefaultColLabelSize() * 2)
-        grid.AutoSize()
-
         grid.EnableDragGridSize(False)
-
-
-        grid.AppendRows(len(events))
-        for row,event in enumerate(events):
-            for col in range(num_cols):
-                grid.SetReadOnly( row, col );
-
-            localtime = time.localtime(event.t.to_sec())
-            time_str = time.strftime("%I:%M.%S %p", localtime)
-            date_str = time.strftime("%a, %b %d %Y", localtime)
-            child_count = EventViewerFrame.sumChildren(event)
-
-            grid.SetCellValue(row,1,date_str)
-            grid.SetCellValue(row,1,date_str)
-            grid.SetCellValue(row,2,time_str)
-            grid.SetCellValue(row,3,event.type)
-            grid.SetCellValue(row,4,event.name)
-            grid.SetCellValue(row,5,str(child_count))
-            grid.SetCellValue(row,6,event.desc)
-            grid.SetCellValue(row,7,str(event.data))
+        self.generateGridData()
         
-        if len(self.events) > 0:
-            self.setReferenceTime(events[0])
-
         grid.AutoSizeColumns()
 
         self.tree = tree = wx.TreeCtrl(self, style=(wx.TR_HIDE_ROOT|wx.TR_HAS_BUTTONS))
         root = tree.AddRoot('ROOT')
-        self.addEventsToTree(events, root)
+        self.addEventGroupsToTree(self.event_groups, root)
         self.Bind(wx.EVT_TREE_SEL_CHANGED, self.onTreeSelectionChanged, self.tree)
+        self.Bind(wx.EVT_TREE_ITEM_EXPANDED, self.onTreeItemExpanded, self.tree)
+        self.Bind(wx.EVT_TREE_ITEM_COLLAPSED, self.onTreeItemCollapsed, self.tree)
+
 
         hsizer2 = wx.BoxSizer(wx.HORIZONTAL)
         hsizer2.Add(self.tree, 2, wx.EXPAND)
@@ -207,12 +227,7 @@ class EventViewerFrame(wx.Frame):
         self.SetSizer(vsizer)
         self.SetAutoLayout(1)
         vsizer.Fit(self)
-
-        self.current_event_selection = None
         
-        if len(self.events) == 0:
-            displayErrorDialog(self, "No events")
-
         self.Show(True)
 
     @staticmethod
@@ -222,47 +237,90 @@ class EventViewerFrame(wx.Frame):
             sum += 1 + EventViewerFrame.sumChildren(child)
         return sum
 
-    def addEventsToTree(self, events, subtree):
-        for event in events:
+    def generateGridData(self):
+        self.visible_event_groups = []
+        for event_group in self.event_groups:
+            self.visible_event_groups += event_group.generateVisibleList()
+                    
+        row_change = len(self.visible_event_groups) - self.grid.GetNumberRows() 
+        if row_change > 0:
+            self.grid.AppendRows(row_change)
+        elif row_change < 0:
+            self.grid.DeleteRows(0, -row_change)
+
+        for row_index,event_group in enumerate(self.visible_event_groups):
+            row_data = event_group.generateGridData(self.ref_time)
+            for col_index,cell_data in enumerate(row_data):
+                self.grid.SetCellValue(row_index,col_index,cell_data)
+                self.grid.SetReadOnly( row_index, col_index );
+
+        self.grid.AutoSize()
+
+
+    def addEventGroupsToTree(self, event_groups, subtree):
+        for event_group in event_groups:
+            event = event_group.event
             desc = event.type + " : " + event.name
             new_subtree = self.tree.AppendItem(subtree, desc)
-            self.tree.SetPyData(new_subtree, event)
-            self.addEventsToTree(event.children, new_subtree)
+            self.tree.SetPyData(new_subtree, event_group)
+            self.addEventGroupsToTree(event_group.subgroups, new_subtree)
+
 
     def onSelectGridCell(self, event):
-        self.current_event_selection = self.events[event.GetRow()]
-        #print "current selection = ", self.current_selection
+        self.current_event_group_selection = self.visible_event_groups[event.GetRow()]
         event.Skip()
 
     def onTreeSelectionChanged(self, event):
         item = event.GetItem()
         if item:
-            self.current_event_selection = self.tree.GetPyData(item)
+            self.current_event_group_selection = self.tree.GetPyData(item)
+
+    def onTreeItemExpanded(self, event):
+        item = event.GetItem()
+        if item:
+            self.tree.GetPyData(item).showChildren(True)
+            self.generateGridData()
+
+    def onTreeItemCollapsed(self, event):
+        item = event.GetItem()
+        if item:
+            self.tree.GetPyData(item).showChildren(False)
+            self.generateGridData()
 
     def onSetReferenceTime(self, event):
-        if self.current_event_selection is None:
+        if self.current_event_group_selection is None:
             displayErrorDialog(self, "Please select a cell or tree item")
         else:
-            self.setReferenceTime(self.current_event_selection)
+            self.ref_time = self.current_event_group_selection.event.t
+            self.generateGridData()
 
-    def setReferenceTime(self, ref_event):
-        grid = self.grid
-        reft = ref_event.t
-        for row,event in enumerate(self.events):
-            duration_str = pretty_duration((event.t-reft).to_sec())
-            grid.SetCellValue(row,0,duration_str)
-        grid.AutoSizeColumns()
+    def onTreeItemRightClick(self, event):
+        print event
+
+    def RightClickCb( self, event ):
+        # record what was clicked
+        self.list_item_clicked = right_click_context = event.GetText()
+
+        ### 2. Launcher creates wxMenu. ###
+        menu = wxMenu()
+        for (id,title) in menu_title_by_id.items():
+            ### 3. Launcher packs menu with Append. ###
+            menu.Append( id, title )
+            ### 4. Launcher registers menu handlers with EVT_MENU, on the menu. ###
+            EVT_MENU( menu, id, self.MenuSelectionCb )
+
+        ### 5. Launcher displays menu with call to PopupMenu, invoked on the source component, passing event's GetPoint. ###
+        self.frame.PopupMenu( menu, event.GetPoint() )
+        menu.Destroy() # destroy to avoid mem leak
 
     def onViewChildren(self, event):
-        if self.current_event_selection is None:
+        if self.current_event_group_selection is None:
             displayErrorDialog(self, "Please select a cell from a given row")
         else:
-            #print "current selection = ", self.current_selection
-            if len(self.current_event_selection.children) == 0:
-                displayErrorDialog(self, "Event has no children")
-            else:
-                viewer = EventViewerFrame(self.current_event_selection.children, self.input_filename)
-                viewer.Raise()
+            self.current_event_group_selection.showChildren(True)
+            self.generateGridData()
+            #viewer = EventViewerFrame(self.current_event_selection.children, self.input_filename)
+            #viewer.Raise()
 
     def onQuit(self, event):
         self.Close(True)
